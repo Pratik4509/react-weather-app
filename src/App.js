0632419f4a9158de7a136b00b91098dbc4ef2500
Pratik <pratikl2614@gmail.com>
2022-05-22 18:24:47 +0530

    Updated details section

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ const App = () => {
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
   
-  const switchTheme = () =>{
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-  }
+  // const switchTheme = () =>{
+  //   const newTheme = theme === 'light' ? 'dark' : 'light';
+  //   setTheme(newTheme);
+  // }
 
   // const [errorMessage, setErrorMessage] = useState();
 
